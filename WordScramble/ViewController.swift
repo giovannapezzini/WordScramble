2c9112409cf4653738d0bd93baca7832dcb95257
giovannapezzini <giovannarpezzini@gmail.com>
2021-01-09 03:04:46 -0300

    Add promptForAnswer

--- WordScramble/ViewController.swift
+++ WordScramble/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Word")
         
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
@@ -30,6 +32,25 @@ class ViewController: UITableViewController {
         }
         
         startGame()
+    }
+    
+    @objc func promptForAnswer() {
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            [weak self, weak ac] _ in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submit(answer)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+        
+    }
+    
+    func submit(_ answer : String) {
+        
     }
     
     func startGame() {

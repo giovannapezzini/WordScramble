5329804aa90d0a1a2cd1c064987ebbe78d443b2a
giovannapezzini <giovannarpezzini@gmail.com>
2021-01-09 00:28:38 -0300

    Initial Commit

---
+++ WordScramble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordScramble
+//
+//  Created by Giovanna Pezzini on 09/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
